f0357157721c4f58fecb5275d8c8c39421669dd5
luc <luc.bourrat.42@gmail.com>
2023-03-23 01:32:31 +0100

    Chapitre 3, La syntaxe JSX

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 let n = 0;
 
+function numberFormat(n) {
+    return n.toString().padStart(2, '0');
+}
+
 function render() {
-    const title = React.createElement('h1', {}, 
-        'Bonjour tout le monde ', 
-        React.createElement('span', {}, n)
-    );
+    // const title = <h1>
+    //     Bonjour les gens <span>{n % 2 ? numberFormat(n) : null}</span>
+    // </h1>
+
+    // const title = <h1>
+    //     Bonjour les gens <span>{['azeaze', '=azeaze']}</span>
+    // </h1>
+
+    // const title = <h1 className="title" id={"title" + n}>
+    //     Bonjour les gens <span>{['azeaze', '=azeaze']}</span>
+    // </h1>
+
+
+    const items = [
+        'Tache 1',
+        'Tache 2',
+        'Tache 3'
+    ]
+    
+    // const lis = items.map(item => <li>{item}</li>);
+    const lis = items.map((item, k) => <li key={k}>{item}</li>);
+
+    const title = <React.Fragment>
+        <h1 className="title" id="title">
+            Bonjour les gens <span>{n}</span>
+        </h1>
+        <ul>{lis}</ul>
+    </React.Fragment>
 
     ReactDOM.render(title, document.querySelector('#app'));
 }
