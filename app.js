5cb3940820e58b527022ecd680c6182683d9b62a
luc <luc.bourrat.42@gmail.com>
2023-03-31 18:40:45 +0200

    Chapitre 4, Notre premier composant

--- app.js
+++ app.js
@@ -1,49 +1,99 @@
-let n = 0;
-
-function numberFormat(n) {
-    return n.toString().padStart(2, '0');
+function WelcomeFunc({name, children}) {
+    return <div>
+        <h1>Bonjour {name}</h1>
+        <p>
+            {children}
+        </p>
+    </div>
 }
 
-function render() {
-    // const title = <h1>
-    //     Bonjour les gens <span>{n % 2 ? numberFormat(n) : null}</span>
-    // </h1>
+class Welcome extends React.Component {
 
-    // const title = <h1>
-    //     Bonjour les gens <span>{['azeaze', '=azeaze']}</span>
-    // </h1>
+    // constructor  (props) {
+    //     super(props)
+    //     console.log(props)
+    // }
 
-    // const title = <h1 className="title" id={"title" + n}>
-    //     Bonjour les gens <span>{['azeaze', '=azeaze']}</span>
-    // </h1>
-
-
-    const items = [
-        'Tache 1',
-        'Tache 2',
-        'Tache 3'
-    ]
-    
-    // const lis = items.map(item => <li>{item}</li>);
-    const lis = items.map((item, k) => <li key={k}>{item}</li>);
-
-    const title = <React.Fragment>
-        <h1 className="title" id="title">
-            Bonjour les gens <span>{n}</span>
-        </h1>
-        <ul>{lis}</ul>
-    </React.Fragment>
-
-    ReactDOM.render(title, document.querySelector('#app'));
+    render() {
+        // console.log(this.props)
+        return <div>
+            <h1>Bonjour {this.props.name}</h1>
+            <p>
+                {this.props.children}
+            </p>
+        </div>
+    }
 }
 
-function render2() {
-    document.querySelector('#app').innerHTML = `<h1>Bonjour tout le monde <span>${n}</span></h1>`;
+class Clock extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {date: new Date()}
+        this.timer = null
+    }
+
+    componentDidMount () {
+        this.timer = window.setInterval(this.tick.bind(this), 1000)
+    }
+
+    componentWillDidUnmount () {
+        window.clearInterval(this.timer)
+    }
+
+    tick () {
+        this.setState({date: new Date()})
+    }
+
+    render () {
+        const date = new Date()
+        return <div>
+            Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+        </div>
+    }
 }
 
-render();
+class Incrementer extends React.Component {
 
-window.setInterval(() => {
-    n++;
-    render()
-}, 1000)
+    constructor (props) {
+        super(props)
+        this.state = {n: props.start}
+        this.timer = null
+    }
+
+    componentDidMount () {
+        window.setInterval(this.increment.bind(this), 1000)
+    }
+
+    componentWillDidUnmount () {
+        window.clearInterval(this.timer)
+    }
+
+    increment () {
+        // this.setState({n: this.state.n + 1})
+        this.setState(function (state, props) {
+            return {n: state.n + props.step}
+        })
+    }
+
+    render () {
+        return <div>Valeur : {this.state.n}</div>
+    }
+}
+
+Incrementer.defaultProps = {
+    start: 0,
+    step: 1,
+}
+
+function Home() {
+    return <div>
+        <Welcome name="Dorothée" />
+        <Welcome name="Jean" />
+        <Clock/>
+        <Incrementer start={10}/>
+        <Incrementer start={100} step={10}/>
+    </div>
+}
+
+ReactDOM.render(<Home/>, document.querySelector('#app'))
